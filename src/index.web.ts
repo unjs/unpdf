97bdfef9b36630bedf8590ac810fde53689e4246
Johann Schopplich <mail@johannschopplich.com>
2023-08-11 23:29:37 +0200

    refactor: simplify browser context fn

--- src/index.web.ts
+++ src/index.web.ts
@@ -1,8 +1,5 @@
-import type { PDFContent } from './types'
 import type { decodePDFText as _decodePDFText } from './index.node'
 
-export async function decodePDFText(
-  ..._args: Parameters<typeof _decodePDFText>
-): Promise<PDFContent> {
+export const decodePDFText: typeof _decodePDFText = async () => {
   throw new Error('Not supported in browser context yet')
 }
