2e818cd6ea7948d4186bda4be0fc5a586345816a
Johann Schopplich <mail@johannschopplich.com>
2023-08-12 22:35:08 +0200

    feat!: `getPDFMeta`

---
+++ src/meta.ts
@@ -0,0 +1,11 @@
+import { getDocumentProxy } from './utils'
+
+export async function getPDFMeta(data: ArrayBuffer) {
+  const pdf = await getDocumentProxy(data)
+  const meta = await pdf.getMetadata().catch(() => null)
+
+  return {
+    info: meta?.info as Record<string, any>,
+    metadata: meta?.metadata?.getAll(),
+  }
+}
